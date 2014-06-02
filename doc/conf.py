c4e3cb875cca2b50ec1c61a7ad17b1cf3b7c69e7
Liam Wickins <liamw9534@gmail.com>
2014-06-02 11:17:27 +0100

    Removed unused seqdiag module.

--- doc/conf.py
+++ doc/conf.py
@@ -29,11 +29,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
-              'sphinx.ext.viewcode', 'sphinx.ext.inheritance_diagram',
-              'sphinxcontrib.seqdiag']
-
-# Fontpath for seqdiag (truetype font)
-seqdiag_fontpath = '/usr/share/fonts/truetype/freefont/FreeMono.ttf'
+              'sphinx.ext.viewcode', 'sphinx.ext.inheritance_diagram']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
